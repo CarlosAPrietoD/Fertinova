d3660a3cbf62bf4275e6592ed9f3d5a8cad85161
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-06-19 17:43:18 -0500

    [ERR] Verificación de contexto y cambio de cifras negativas

--- fertinova_addons/models/stock_move_inherited.py
+++ fertinova_addons/models/stock_move_inherited.py
@@ -2,6 +2,8 @@
 import math
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
+import logging
+_logger = logging.getLogger(__name__)
 
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
@@ -92,8 +94,7 @@ class StockMoveLine(models.Model):
                            digits=dp.get_precision('Product Unit of Measure'),
                            compute='_get_outputs')     
 
-    transfers = fields.Float(string='Transfers', 
-                             store=True,                             
+    transfers = fields.Float(string='Transfers',                                                          
                              digits=dp.get_precision('Product Unit of Measure'),
                              compute='_get_transfers') 
 
@@ -145,7 +146,7 @@ class StockMoveLine(models.Model):
         if not record.qty_done and not record.x_studio_valor:
           record.inputs = 0.0
         else:
-          #If value is equal or lesser than 0 "inputs" must be 0.0    
+          #If value is equal or lesser than 0 "inputs" must be 0.0:    
           if record.x_studio_valor > 0:
             record.inputs = record.qty_done  
           else:
@@ -156,23 +157,31 @@ class StockMoveLine(models.Model):
     def _get_outputs(self):
       '''This method computes the value of outputs'''
       for record in self:
-        #If value is equal or superior than 0 "inputs" must be 0.0    
+        #If value is equal or superior than 0 "inputs" must be 0.0:    
         if record.x_studio_valor >= 0:
           record.outputs = 0.0  
         else:
           record.outputs = record.qty_done
     
 
-    @api.depends('qty_done', 'x_studio_valor')
+    @api.depends('location_id', 'qty_done', 'x_studio_valor')
     def _get_transfers(self):
       '''This method computes the value of transfers'''
-      for record in self:
-        #If value is equal to 0 "tranfers" must be qty_done  
-        if record.x_studio_valor == 0.0000 or record.x_studio_valor == 0 or not record.x_studio_valor or record.x_studio_valor == None:            
-          record.transfers = record.qty_done
+      # Since 0.0, 0, None are evaluated as False
+      _logger.info('\n\n\n contexto en st.mv.ln: %s\n\n\n', str(self.env.context))         
+      for record in self: 
+        #If value is equal to 0 "tranfers" must be qty_done:  
+        _logger.info('\n\n\n fuera del condicional valor: %s\n\n\n', record.x_studio_valor)        
+        if not record.x_studio_valor: 
+          _logger.info('\n\n\n dentro del condicional valor: %s\n\n\n', record.x_studio_valor)                                 
+          #Although it is important notice the conditional about the context brought from 'stock.quant'             
+          if self.env.context.get('location_id') == record.location_id.id:        
+            record.transfers = record.qty_done  * -1  
+          else:
+            record.transfers = record.qty_done                    
         else:
           record.transfers = 0.0
-
+                                  
 
     #@api.depends('product_id', 'operative_qty')
     #def _get_accumulated_qty(self):
@@ -201,7 +210,7 @@ class StockMoveLine(models.Model):
         if not record.qty_done:
           record.price_unit = 0.0
         else:
-          #price unit = value / quantity done                
+          #price unit = value / quantity done :               
           record.price_unit = record.x_studio_valor / record.qty_done      
       
     
---
+++ fertinova_addons/models/stock_quant_inherited.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+# TICKET 107 DEVELOPED BY SEBASTIAN MENDEZ -- START
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    def action_view_stock_moves(self):
+        """This method intends to extend the original method "action_view_stock_moves()" in order to pass the location_id selected in "stock.quant" by the context.
+        This is made by entering into STOCK > MAIN DATA {Menu} > PRODUCTS {Menuitem} > a given product > Smart Button {action_open_quants}"""
+        
+        #Call in order to extend method 'action_view_stock_moves' 
+        action = super(StockQuant, self).action_view_stock_moves()
+
+        #Pass the location_id by context:
+        ctx = eval(action['context'])
+        ctx.update({'location_id': self.location_id.id})
+        action['context'] = str(ctx)    
+        _logger.info('\n\n\n contexto en st.qnt: %s\n\n\n', str(self.env.context))         
+
+        return action         
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+# TICKET 107 DEVELOPED BY SEBASTIAN MENDEZ -- END
+#//////////////////////////////////////////////////////////////////////////////////////////////#
