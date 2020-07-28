1ff94a507ae6583f5d00cd675476bd2dd3d9c371
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-07-28 10:29:15 -0500

    [ERR] Errores en métodos, cambios en recordsets, consultas, iteraciones

--- fertinova_addons/models/product_product_inherited.py
+++ fertinova_addons/models/product_product_inherited.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
+import logging
+_logger = logging.getLogger(__name__)
+ 
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 #   PRACTICE    DEVELOPED BY SEBASTIAN MENDEZ    --     START
@@ -11,34 +14,37 @@ class ProductProduct(models.Model):
     #:::::::::::::::::::::::::::::::::::::::
     # MODEL FIELDS
     #::::::::::::::::::::::::::::::::::::::: 
-    inputs = fields.Float(string='Inputs', 
-                          store=True,
-                          digits=dp.get_precision('Product Unit of Measure'),
-                          compute='_get_inputs') 
-
-    outputs = fields.Float(string='Outputs', 
-                           store=True,
-                           digits=dp.get_precision('Product Unit of Measure'),
-                           compute='_get_outputs')     
-
-    #::::::::::::::::::::::::::::::::::::
-    # MODEL METHODS
-    #::::::::::::::::::::::::::::::::::::
-    def _get_inputs(self):
-      """This method computes the value of inputs"""
-      for record in self:
-        #Retrieval of inputs from model 'stock.move.line' matching with id->product_id:         
-        sql_query = """SELECT sum(inputs) FROM stock_move_line WHERE product_id = %s;"""
-        self.env.cr.execute(sql_query, (record.id,))
-        inputs_aux = self.env.cr.fetchone()
-        record.inputs = inputs_aux[0]
+    inputs = fields.Float(string='Inputs', digits=dp.get_precision('Product Unit of Measure')) 
+    outputs = fields.Float(string='Outputs', digits=dp.get_precision('Product Unit of Measure'))     
 
 
-    def _get_outputs(self):
-      """This method computes the value of inputs"""
-      for record in self:
-        #Retrieval of outputs from model 'stock.move.line' matching with id->product_id:         
-        sql_query = """SELECT sum(outputs) FROM stock_move_line WHERE product_id = %s;"""
-        self.env.cr.execute(sql_query, (record.id,))
-        outputs_aux = self.env.cr.fetchone()
-        record.outputs = outputs_aux[0]
+class StockQuantityHistory(models.TransientModel):
+    _inherit = 'stock.quantity.history'
+
+    def open_table(self):      
+      date_ingress = self.date
+      _logger.info('\n\n\n date: %s\n\n\n', date_ingress)  
+      _logger.info('\n\n\n contexto: %s\n\n\n', self.env.context) 
+
+      #Verify if radiobutton or option "compute_at_date" has been selected:
+      if self.compute_at_date:
+
+        #Inheritance of original method "open_table()" 
+        action = super(StockQuantityHistory, self).open_table()
+      
+        #Obtain all products from model "product.product"
+        products_all = self.env['product.product'].search([('type', '=', 'product')])
+        _logger.info('\n\n\n stk_mv_lns: %s\n\n\n', products_all)  
+
+        for product in products_all.ids:
+          #Retrieval of inputs from model 'stock.move.line' matching with id->product_id:         
+          sql_query = """SELECT sum(inputs) FROM stock_move_line WHERE product_id = (product,) AND date <= (self.date,);"""
+          self.env.cr.execute(sql_query)
+          inputs_aux = self.env.cr.fetchone()  
+          _logger.info('\n\n\n inputs_aux: %s\n\n\n', inputs_aux)          
+
+          #Modify the recordset for a given product:
+          recordset_product = self.env['product.product'].browse(product) 
+          recordset_product.inputs = inputs_aux[0]         
+
+      return action   
