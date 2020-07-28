4f402fd9d3ed4f78a6948a571293eee0f91737a8
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-07-28 12:03:27 -0500

    [ADD] Agregación de traducciones, correcciones en métodos

--- fertinova_addons/models/product_product_inherited.py
+++ fertinova_addons/models/product_product_inherited.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
-import logging
-_logger = logging.getLogger(__name__)
  
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
@@ -22,29 +20,38 @@ class StockQuantityHistory(models.TransientModel):
     _inherit = 'stock.quantity.history'
 
     def open_table(self):      
+      #Assign date input by user:
       date_ingress = self.date
-      _logger.info('\n\n\n date: %s\n\n\n', date_ingress)  
-      _logger.info('\n\n\n contexto: %s\n\n\n', self.env.context) 
 
       #Verify if radiobutton or option "compute_at_date" has been selected:
       if self.compute_at_date:
 
-        #Inheritance of original method "open_table()" 
+        #INHERITANCE of original method "open_table()": 
         action = super(StockQuantityHistory, self).open_table()
       
-        #Obtain all products from model "product.product"
-        products_all = self.env['product.product'].search([('type', '=', 'product')])
-        _logger.info('\n\n\n stk_mv_lns: %s\n\n\n', products_all)  
+        #Obtain all products from model "product.product":
+        all_products = self.env['product.product'].search([('type', '=', 'product')]) 
 
-        for product in products_all.ids:
-          #Retrieval of inputs from model 'stock.move.line' matching with id->product_id:         
-          sql_query = """SELECT sum(inputs) FROM stock_move_line WHERE product_id = (product,) AND date <= (self.date,);"""
-          self.env.cr.execute(sql_query)
+        for product in all_products.ids:
+          #Retrieve INputs summatory from model "stock.move.line" 
+          # matching with id->product_id and a given date:         
+          sql_query = """SELECT sum(inputs) FROM stock_move_line WHERE product_id = %(prod)s AND date <= %(date)s;"""
+          self.env.cr.execute(sql_query, {'prod': product, 'date': date_ingress})
           inputs_aux = self.env.cr.fetchone()  
-          _logger.info('\n\n\n inputs_aux: %s\n\n\n', inputs_aux)          
 
-          #Modify the recordset for a given product:
+          #Retrieve OUTputs summatory from model "stock.move.line" 
+          # matching with id->product_id and a given date:         
+          sql_query = """SELECT sum(outputs) FROM stock_move_line WHERE product_id = %(prod)s AND date <= %(date)s;"""
+          self.env.cr.execute(sql_query, {'prod': product, 'date': date_ingress})
+          outputs_aux = self.env.cr.fetchone()                    
+
+          #Modify recordset according to iterated product to set inputs and outputs:
           recordset_product = self.env['product.product'].browse(product) 
-          recordset_product.inputs = inputs_aux[0]         
+          recordset_product.inputs  = inputs_aux[0]         
+          recordset_product.outputs = outputs_aux[0]
 
-      return action   
+      #Return modified view in method "open_table" :
+      return action 
+#//////////////////////////////////////////////////////////////////////////////////////////////#
+#   PRACTICE    DEVELOPED BY SEBASTIAN MENDEZ    --     END
+#//////////////////////////////////////////////////////////////////////////////////////////////#        
