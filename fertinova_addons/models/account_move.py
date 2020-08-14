a2a04ca9c16c0e37bea2cf0962717af2bb971b71
Noe Moreno <nmoreno@wobin.com.mx>
2020-08-14 12:17:12 -0500

    [FIX] cambio datos factura por po y so

--- fertinova_addons/models/account_move.py
+++ fertinova_addons/models/account_move.py
@@ -10,41 +10,45 @@ class AccountMove(models.Model):
         array_lines = []
 
         for line in self.line_ids:
-            if line.purchase and line.po_status and line.po_date:
-                if line.po_status == 'paid':
-                    tag_lines = []
-                    for tag in line.analytic_tag_ids:
-                        tag_lines.append((4,tag.id))
-                    line_vals = {
-                            'purchase': line.purchase.id,
-                            'po_status' : line.po_status,
-                            'po_date' : line.po_date,
-                            'account_id' : line.account_id.id,
-                            'debit' : line.credit,
-                            'partner_id' : line.partner_id.id,
-                            'analytic_account_id' : line.analytic_account_id.id,
-                            'analytic_tag_ids' : tag_lines
-                            }
+            if line.purchase and line.po_status:
+                tag_lines = []
+                for tag in line.analytic_tag_ids:
+                    tag_lines.append((4,tag.id))
+                date_inv = False
+                if line.purchase.invoice_ids:
+                    date_inv=line.purchase.invoice_ids[0].date_invoice
+                line_vals = {
+                        'purchase': line.purchase.id,
+                        'po_status' : line.purchase.invoice_status,
+                        'po_date' : date_inv,
+                        'account_id' : line.account_id.id,
+                        'credit' : line.debit,
+                        'partner_id' : line.partner_id.id,
+                        'analytic_account_id' : line.analytic_account_id.id,
+                        'analytic_tag_ids' : tag_lines
+                        }
 
-                    array_lines.append((0,0,line_vals))
+                array_lines.append((0,0,line_vals))
             
-            if line.sale and line.so_status and line.so_date:
-                if line.so_status == 'paid':
-                    tag_lines = []
-                    for tag in line.analytic_tag_ids:
-                        tag_lines.append((4,tag.id))
-                    line_vals = {
-                            'sale': line.sale.id,
-                            'so_status' : line.so_status,
-                            'so_date' : line.so_date,
-                            'account_id' : line.account_id.id,
-                            'credit' : line.debit,
-                            'partner_id' : line.partner_id.id,
-                            'analytic_account_id' : line.analytic_account_id.id,
-                            'analytic_tag_ids' : tag_lines
-                            }
+            if line.sale and line.so_status:
+                tag_lines = []
+                for tag in line.analytic_tag_ids:
+                    tag_lines.append((4,tag.id))
+                date_inv = False
+                if line.sale.invoice_ids:
+                    date_inv=line.sale.invoice_ids[0].date_invoice
+                line_vals = {
+                        'sale': line.sale.id,
+                        'so_status' : line.sale.invoice_status,
+                        'so_date' : date_inv,
+                        'account_id' : line.account_id.id,
+                        'debit' : line.credit,
+                        'partner_id' : line.partner_id.id,
+                        'analytic_account_id' : line.analytic_account_id.id,
+                        'analytic_tag_ids' : tag_lines
+                        }
 
-                    array_lines.append((0,0,line_vals))
+                array_lines.append((0,0,line_vals))
         
         vals = {
                 'ref': self.ref,
@@ -67,21 +71,18 @@ class AccountMoveLine(models.Model):
 
     purchase = fields.Many2one('purchase.order', string="Purchase Order")
     po_status = fields.Selection([
-            ('draft','Draft'),
-            ('open', 'Open'),
-            ('in_payment', 'In Payment'),
-            ('paid', 'Paid'),
-            ('cancel', 'Cancelled'),
-        ],string="PO invoice status")
+        ('no', 'Nothing to Bill'),
+        ('to invoice', 'Waiting Bills'),
+        ('invoiced', 'No Bill to Receive'),
+    ],string="PO invoice status")
     po_date = fields.Date(string="PO invoice date")
     sale = fields.Many2one('sale.order', string="Sale Order")
     so_status = fields.Selection([
-            ('draft','Draft'),
-            ('open', 'Open'),
-            ('in_payment', 'In Payment'),
-            ('paid', 'Paid'),
-            ('cancel', 'Cancelled'),
-        ],string="SO invoice status")
+        ('no', 'Nothing to Bill'),
+        ('to invoice', 'Waiting Bills'),
+        ('invoiced', 'No Bill to Receive'),
+        ('upselling', 'Upselling Opportunity')
+    ],string="SO invoice status")
     so_date = fields.Date(string="SO invoice date")
     prov = fields.Boolean(related='move_id.prov')
 
@@ -90,31 +91,22 @@ class AccountMoveLine(models.Model):
     def get_account_po(self):
         if self.purchase:
             if self.purchase.invoice_ids:
-                self.po_status = self.purchase.invoice_ids[0].state
                 self.po_date = self.purchase.invoice_ids[0].date_invoice
-                self.credit = self.purchase.invoice_ids[0].amount_total
-                self.partner_id = self.purchase.invoice_ids[0].partner_id
-                n_account=-1
-                if self.purchase.invoice_ids[0].invoice_line_ids:
-                    n_line=-1
-                    for line in self.purchase.invoice_ids[0].invoice_line_ids:
-                        n_line+=1
-                        if line.account_id:
-                            code = line.account_id.code
-                            if code[0] == '2':
-                                n_account=n_line
-                if n_account != -1:
-                    self.account_id = self.purchase.invoice_ids[0].invoice_line_ids[0].account_id
-                    self.analytic_account_id = self.purchase.invoice_ids[0].invoice_line_ids[0].account_analytic_id
-                    self.analytic_tag_ids = self.purchase.invoice_ids[0].invoice_line_ids[0].analytic_tag_ids
-            else:
-                self.account_id = False
-                self.po_status = False
-                self.po_date = False
-                self.credit = False
-                self.partner_id = False
-                self.analytic_account_id = False
-                self.analytic_tag_ids = False
+            self.po_status = self.purchase.invoice_status
+            self.debit = self.purchase.amount_total
+            self.partner_id = self.purchase.partner_id
+            n_account=-1
+            if self.purchase.order_line:
+                n_line=-1
+                for line in self.purchase.order_line:
+                    n_line+=1
+                    if line.product_id:
+                        n_account=n_line
+            if n_account != -1:
+                self.account_id = self.purchase.order_line[n_account].product_id.categ_id.property_account_expense_categ_id
+                self.analytic_account_id = self.purchase.order_line[n_account].account_analytic_id
+                self.analytic_tag_ids = self.purchase.order_line[n_account].analytic_tag_ids
+           
         else:
             self.account_id = False
             self.po_status = False
@@ -124,37 +116,28 @@ class AccountMoveLine(models.Model):
             self.analytic_account_id = False
             self.analytic_tag_ids = False
 
+
     
     @api.multi
     @api.onchange('sale')
     def get_account_so(self):
         if self.sale:
             if self.sale.invoice_ids:
-                self.so_status = self.sale.invoice_ids[0].state
                 self.so_date = self.sale.invoice_ids[0].date_invoice
-                self.debit = self.sale.invoice_ids[0].amount_total
-                self.partner_id = self.sale.invoice_ids[0].partner_id
-                n_account=-1
-                if self.sale.invoice_ids[0].invoice_line_ids:
-                    n_line=-1
-                    for line in self.sale.invoice_ids[0].invoice_line_ids:
-                        n_line+=1
-                        if line.account_id:
-                            code = line.account_id.code
-                            if code[0] == '4':
-                                n_account=n_line
-                if n_account != -1:
-                    self.account_id = self.sale.invoice_ids[0].invoice_line_ids[n_account].account_id
-                    self.analytic_account_id = self.sale.invoice_ids[0].invoice_line_ids[n_account].account_analytic_id
-                    self.analytic_tag_ids = self.sale.invoice_ids[0].invoice_line_ids[n_account].analytic_tag_ids
-            else:
-                self.account_id = False
-                self.so_status = False
-                self.so_date = False
-                self.debit = False
-                self.partner_id = False
-                self.analytic_account_id = False
-                self.analytic_tag_ids = False
+            self.so_status = self.sale.invoice_status
+            self.credit = self.sale.amount_total
+            self.partner_id = self.sale.partner_id
+            n_account=-1
+            if self.sale.order_line:
+                n_line=-1
+                for line in self.sale.order_line:
+                    n_line+=1
+                    if line.product_id:
+                        n_account=n_line
+            if n_account != -1:
+                self.account_id = self.sale.order_line[n_account].product_id.categ_id.property_account_expense_categ_id
+                #self.analytic_account_id = self.sale.order_line[n_account].account_analytic_id
+                self.analytic_tag_ids = self.sale.order_line[n_account].analytic_tag_ids
         else:
             self.account_id = False
             self.so_status = False
