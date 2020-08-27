30fe434634fdb4bdb551d487d9f83c9504d13db5
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 12:50:56 -0500

    Update payments.py

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -8,14 +8,8 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
     
     cta_diario_pago = fields.Char(string='Cuenta Diario Pago')
-    factura = fields.Many2one('account.invoice', 
-                              string = 'Factura',
-                              domain = [('partner_id', '=', self.partner_id.id),
-                                        ('state', 'not in', ['paid', 'cancel'])])
-    compra = fields.Many2one('purchase.order', 
-                              string = 'Orden de Compra',
-                              domain = [('partner_id', '=', self.partner_id.id),
-                                        ('state', 'not in', ['done', 'cancel'])]) 
+    factura = fields.Many2one('account.invoice', string='Factura') #domain=[('partner_id', '=', self.partner_id.id), ('state', 'not in', ['paid', 'cancel'])])
+    compra = fields.Many2one('purchase.order', string='Orden de Compra') #domain = [('partner_id', '=', self.partner_id.id), ('state', 'not in', ['done', 'cancel'])]) 
 
     @api.onchange('journal_id')
     def _onchange_journal_id(self):
