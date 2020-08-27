938dd2dbd0084e5193ac7369521f1b785cebc5f9
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 13:12:06 -0500

    Update payments.py

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -7,7 +7,7 @@ from odoo import models, fields, api
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
     
-    cta_diario_pago = fields.Char(string='Cuenta Diario Pago')
+    cta_diario_pago = fields.Many2one('res.partner.bank', string='Cuenta Diario Pago')
     factura = fields.Many2one('account.invoice', string='Factura') #domain=[('partner_id', '=', self.partner_id.id), ('state', 'not in', ['paid', 'cancel'])])
     compra = fields.Many2one('purchase.order', string='Orden de Compra') #domain = [('partner_id', '=', self.partner_id.id), ('state', 'not in', ['done', 'cancel'])]) 
 
