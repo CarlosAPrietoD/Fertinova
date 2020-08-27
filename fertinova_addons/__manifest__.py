eac48b011ae3343e447a0c73bf92cd3fe4844988
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 12:37:13 -0500

    Update __manifest__.py

--- fertinova_addons/__manifest__.py
+++ fertinova_addons/__manifest__.py
@@ -19,13 +19,13 @@
         'security/account_security.xml',     
         
         #Views:
-        'views/account_move.xml',        
+        #'views/account_move.xml',        
         #'views/account_payments_inherited_view.xml',         
-        'views/product_template_inherited_view.xml',
-        'views/purchase_inherited_view.xml',
-        'views/sale_inherited_view.xml',       
-        'views/stock_move_inherited_view.xml',
-        'views/stock_picking_inherited_view.xml',
+        #'views/product_template_inherited_view.xml',
+        #'views/purchase_inherited_view.xml',
+        #'views/sale_inherited_view.xml',       
+        #'views/stock_move_inherited_view.xml',
+        #'views/stock_picking_inherited_view.xml',
 
         #Wizards:
         'wizard/account_move_inherited_view.xml',
