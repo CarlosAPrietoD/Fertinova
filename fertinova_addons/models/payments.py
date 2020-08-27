2d66f4c623d4027833e1a840d5b26c1823c0645e
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 13:48:49 -0500

    Update payments.py

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -7,10 +7,23 @@ from odoo import models, fields, api
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
     
-    cta_diario_pago = fields.Many2one('res.partner.bank', string='Cuenta Diario Pago')
-    factura = fields.Many2one('account.invoice', string='Factura') #domain=[('partner_id', '=', self.partner_id.id), ('state', 'not in', ['paid', 'cancel'])])
-    compra = fields.Many2one('purchase.order', string='Orden de Compra') #domain = [('partner_id', '=', self.partner_id.id), ('state', 'not in', ['done', 'cancel'])]) 
+    bank_account_id = fields.Many2one('res.partner.bank', string='Cuenta Diario Pago', compute='_compute_bank_account')
+    invoices_id     = fields.Many2one('account.invoice', string='Factura', compute='_set_invoices')
+    purchases_id    = fields.Many2one('purchase.order', string='Orden de Compra', compute='_set_purchases')
+    
 
-    @api.onchange('journal_id')
-    def _onchange_journal_id(self):
-        self.cta_diario_pago = self.env['account.journal'].search([('id', '=', self.journal_id)]).bank_account_id
+    @api.depends('journal_id')
+    def _compute_bank_account(self):
+        self.bank_account_id = self.env['account.journal'].search([('id', '=', self.journal_id)]).bank_account_id
+        
+        
+    def _set_invoices(self):
+        domain=[('partner_id', '=', self.partner_id.id), 
+                ('state', 'not in', ['paid', 'cancel'])]
+        self.invoices_id = self.env['account.invoice'].search(domain)
+        
+        
+    def _set_purchases(self):
+        domain = [('partner_id', '=', self.partner_id.id), 
+                  ('state', 'not in', ['done', 'cancel'])]
+        self.purchases_id = self.env['purchase.order'].search(domain)
