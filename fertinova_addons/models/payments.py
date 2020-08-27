ffe53823ee89d119cbfce029f82df2baf096dd1d
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 11:00:19 -0500

    Update payments.py

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -2,12 +2,12 @@
 from odoo import models, fields, api
 
 #//////////////////////////////////////////////////////////////////////////////////////////////#
-#   TICKET 035    DEVELOPED BY SEBASTIAN MENDEZ    --     START
+#   TICKET 164    DEVELOPED BY SEBASTIAN MENDEZ    --     START
 #//////////////////////////////////////////////////////////////////////////////////////////////#
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
     
-    cta_diario_pago = fields.Char(string='Cuenta Diario Pago')
+    cta_diario_pago = fields.Many2one('account.journal', string='Cuenta Diario Pago')
     factura = fields.Many2one('account.invoice', 
                               string = 'Factura',
                               domain = [('partner_id', '=', self.partner_id.id),
@@ -15,4 +15,8 @@ class AccountPayment(models.Model):
     compra = fields.Many2one('purchase.order', 
                               string = 'Orden de Compra',
                               domain = [('partner_id', '=', self.partner_id.id),
-                                        ('state', 'not in', ['done', 'cancel'])])
+                                        ('state', 'not in', ['done', 'cancel'])]) 
+
+    @api.onchange('journal_id')
+    def _onchange_journal_id(self):
+        self.cta_diario_pago = self.env['account.journal'].search([('id', '=', self.journal_id)]).bank_account_id
