64e566835edd1d403d16930dc2c520bb80e5e8a6
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-27 16:18:10 -0500

    Update payments.py

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -7,9 +7,9 @@ from odoo import models, fields, api
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
     
-    bank_account_id = fields.Many2one('res.partner.bank', string='Cuenta Diario Pago', compute='_compute_bank_account')
-    invoices_id     = fields.Many2one('account.invoice', string='Factura', compute='_set_invoices')
-    purchases_id    = fields.Many2one('purchase.order', string='Orden de Compra', compute='_set_purchases')
+    bank_account_id = fields.Many2one('res.partner.bank', string='Cuenta Diario Pago', domain=_compute_bank_account)
+    invoices_id     = fields.Many2one('account.invoice', string='Factura', domain=_set_invoices)
+    purchases_id    = fields.Many2one('purchase.order', string='Orden de Compra', domain=_set_purchases)
     
 
     @api.depends('journal_id')
@@ -20,10 +20,10 @@ class AccountPayment(models.Model):
     def _set_invoices(self):
         domain=[('partner_id', '=', self.partner_id.id), 
                 ('state', 'not in', ['paid', 'cancel'])]
-        self.invoices_id = self.env['account.invoice'].search(domain)
+        return domain
         
         
     def _set_purchases(self):
         domain = [('partner_id', '=', self.partner_id.id), 
                   ('state', 'not in', ['done', 'cancel'])]
-        self.purchases_id = self.env['purchase.order'].search(domain)
+        return domain
