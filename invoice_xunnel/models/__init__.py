e5dc53daa8bc0e90e097ee9a192a66afcf8bd12b
Noe Moreno <nmoreno@wobin.com.mx>
2020-09-18 13:19:25 -0500

    [ADD] xunnel

---
+++ invoice_xunnel/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import res_company
+from . import workflow
+from . import account_invoice
+from . import ir_attachment
