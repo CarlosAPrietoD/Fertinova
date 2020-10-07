24588cdc5e716ee319b9eb908090d1be8977f5fc
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-10-07 09:10:48 -0500

    [FIX] Corrección en métodos de state_change y herencia en post()

--- fertinova_addons/models/payments.py
+++ fertinova_addons/models/payments.py
@@ -10,7 +10,7 @@ class AccountPayment(models.Model):
     #\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\
     #             MODEL FIELDS
     #state = fields.Selection(selection_add=[('authorized', 'Autorizado'), ('draft',)])
-    state = fields.Selection(selection=[
+    state_aux = fields.Selection(selection=[
         ('draft', 'Draft'),
         ('authorized', 'Autorizado'),
         ('posted', 'Posted'),
@@ -60,6 +60,14 @@ class AccountPayment(models.Model):
         payment_order_recorset = self.env['account.payment'].browse(self.id)
         payment_order_recorset.message_post(body=payment_post)         
 
+
+
+    @api.multi
+    def post(self):
+        for rec in self:
+            if rec.state == 'authorized': 
+                values = {'state': 'draft'}
+                self.write(values)                                               
     '''        
     @api.multi
     def post(self):
