a615f2018e7160cbf5a3d2591f62b404fd579d90
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-10-26 09:44:41 -0600

    [ERR] Error en generación de name desde sales order al pulsar Confirmar

--- wobin_logistics/models/contracts.py
+++ wobin_logistics/models/contracts.py
@@ -57,9 +57,16 @@ class SaleOrder(models.Model):
         #Normal Logic of method "action_confirm" of Sales Order:
         sale_order = super(SaleOrder, self)._action_confirm()
         #Create a new contract in Wobin Logistics triggered by 
-        #a confirmation in Sales Order:       
-        contract = {'name': self.env['ir.sequence'].next_by_code('self.contract'),
+        #a confirmation in Sales Order:  
+        sequence = self.env['ir.sequence'].next_by_code('self.contract')  
+        #Retrieved  --> CONTR000011
+        numerical_part = int(sequence[5:])
+        numerical_part += 1
+        #After number has increased, fill with zeros to 6 digits
+        sequence_aux = str(numerical_part).zfill(6)
+
+        contract = {'name': 'CONTR' + sequence_aux,
                     'client_id': self.partner_id.id,
                     'sales_order_id': self.id}
-        record = self.env['logistics.contracts'].create(contract)        
+        self.env['logistics.contracts'].create(contract)        
         return sale_order        
