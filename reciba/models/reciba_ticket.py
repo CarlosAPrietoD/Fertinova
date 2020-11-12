37603545932a579bd234b46393b3eadaf860e5a1
Noe Moreno <nmoreno@wobin.com.mx>
2020-11-12 08:57:27 -0600

    [FIX] write

--- reciba/models/reciba_ticket.py
+++ reciba/models/reciba_ticket.py
@@ -297,17 +297,6 @@ class RecibaTicket(models.Model):
 
         self.po_id = purchase.id
 
-    
-
-
-    @api.multi
-    def write(self, values):
-        ticket = super(RecibaTicket, self).write(values)
-
-        if self.state == 'confirmed' and self.price > 0 and self.picking_id:
-            self.picking_id.unlink()
-
-        return ticket
 
 
 class RecibaTicketParams(models.Model):
