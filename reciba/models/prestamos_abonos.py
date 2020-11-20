8bdeb298a5ac2b028647869ff05f3e579f2276b6
WOBIN-simple-cloud <cprieto@wobin.com.mx>
2020-11-20 12:50:58 -0600

    [ERR] Avances, pendientes en metodos de intereses

--- reciba/models/prestamos_abonos.py
+++ reciba/models/prestamos_abonos.py
@@ -6,7 +6,10 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountPayment(models.Model):
-    _inherit = 'account.payment'       
+    _inherit = 'account.payment'      
+
+    def _set_abonos(self):
+        pass     
     
     rec_abono_prestamo   = fields.Char(string='Concepto')
     tipo_movimiento      = fields.Selection([
@@ -15,11 +18,11 @@ class AccountPayment(models.Model):
                                              ('prestamo_acreedor', 'Préstamo a Acreedores Diversos'),
                                              ('abono_acreedor', 'Abono a Acreedores Diversos')
                                             ], string='Tipo de Préstamo/Abono', compute='_set_tipo')
-    abonos_relacionados  = fields.One2many('account.payment', 'id', string='Abonos relacionados')
+    abonos_relacionados  = fields.One2many('account.payment', 'id', string='Abonos relacionados', default=_set_abonos)
     prestamo_relacionado = fields.Many2one('account.payment', string='')
-    dias_transcurridos   = fields.Integer(string='')
-    interes_generado     = fields.Float(string='Interés', digits=(15,2))    
-    saldo_computado      = fields.Float(string='Saldo Computado', digits=(15,2)) 
+    dias_transcurridos   = fields.Integer(string='', compute='_set_dias')
+    interes_generado     = fields.Float(string='Interés', digits=(15,2), compute='_set_interes')    
+    saldo_computado      = fields.Float(string='Saldo Computado', digits=(15,2), compute='_set_saldo_computado') 
 
     @api.one
     @api.depends('journal_id')
@@ -46,3 +49,17 @@ class AccountPayment(models.Model):
         elif self.destination_journal_id.id in journal_id_acreedores.ids and self.journal_id.id in journal_id_banco.ids:                     
             self.tipo_movimiento = 'prestamo_acreedor'                
     
+    
+    @api.one
+    def _set_dias(self):
+        pass
+
+
+    @api.one
+    def _set_interes(self):
+        pass
+
+
+    @api.one
+    def _set_saldo_computado(self):
+        pass
