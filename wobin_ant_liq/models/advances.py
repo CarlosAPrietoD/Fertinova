d2cae951cd14b619de66ff5b5be0fb740b4c9486
WOBIN-simple-cloud <cprieto@wobin.com.mx>
2021-02-19 13:39:38 -0600

    [ADD] Remoción de circuitos, Corrección a viajes, Agregación de ajustes a Liquidaciones, nuevos botones, validaciones, Nueva vista

--- wobin_ant_liq/models/advances.py
+++ wobin_ant_liq/models/advances.py
@@ -8,14 +8,7 @@ class AccountPayment(models.Model):
 
     advance_id      = fields.Many2one('wobin.advances', string='Advance')
     comprobation_id = fields.Many2one('wobin.comprobations', string='Comprobation') 
-
-
-
-class WobinCircuits(models.Model):
-    _name = 'wobin.circuits'
-    _description = 'Wobin Circuits'
-
-    name = fields.Char(string="Circuit to be created")
+    settlement_id   = fields.Many2one('wobin.settlements', string='Settlement')
 
 
 
@@ -43,7 +36,6 @@ class WobinAdvances(models.Model):
         #Create a new record for Wobin Moves Advances Settlements Lines
         values = {
                   'operator_id': res.operator_id.id,
-                  'circuit_id': res.circuit_id.id,
                   'trip_id': res.trip_id.id,
                   'advance_id': res.id
                  }
@@ -52,6 +44,15 @@ class WobinAdvances(models.Model):
         #Set value of id for Wobin Moves Advances Settlements Lines in Advances:
         res.mov_lns_ad_set_id = mov_lns_obj.id 
 
+        #Create a new record for Wobin Settlements:
+        vals_set = {
+                    'name': self.env['ir.sequence'].next_by_code('self.settlement'),  
+                    'operator_id': res.operator_id.id,
+                    'trip_id': res.trip_id.id,
+                    'state': 'pending'
+                   }
+        self.env['wobin.settlements'].create(vals_set)         
+
         return res
 
 
@@ -59,11 +60,11 @@ class WobinAdvances(models.Model):
     operator_id = fields.Many2one('hr.employee',string='Operator', track_visibility='always', ondelete='cascade')
     date        = fields.Date(string='Date', track_visibility='always')
     amount      = fields.Float(string='Amount $', digits=dp.get_precision('Product Unit of Measure'), group_operator=False, track_visibility='always')
-    circuit_id  = fields.Many2one('wobin.circuits', string='Circuit', track_visibility='always', ondelete='cascade')
     trip_id     = fields.Many2one('wobin.logistica.trips', string='Trip', track_visibility='always', ondelete='cascade')
     expenses_to_check  = fields.Float(string='Pending Expenses to Check', digits=dp.get_precision('Product Unit of Measure'), compute='set_expenses_to_check', track_visibility='always')
     payment_related_id = fields.Many2one('account.payment', string='Related Payment', compute='set_related_payment', ondelete='cascade', track_visibility='always')
     mov_lns_ad_set_id  = fields.Many2one('wobin.moves.adv.set.lines', string='Movs Lns Adv Set Id', ondelete='cascade')
+    settlement_id      = fields.Many2one('wobin.settlements', string='Settlement')
 
 
 
@@ -88,11 +89,11 @@ class WobinAdvances(models.Model):
 
     @api.one
     def set_expenses_to_check(self):
-        #Sum amounts from the same circuit by operator
+        #Sum amounts from the same trip by operator
         sql_query = """SELECT sum(amount) 
                          FROM wobin_advances 
-                        WHERE circuit_id = %s AND operator_id = %s"""
-        self.env.cr.execute(sql_query, (self.circuit_id.id, self.operator_id.id,))
+                        WHERE trip_id = %s AND operator_id = %s"""
+        self.env.cr.execute(sql_query, (self.trip_id.id, self.operator_id.id,))
         result = self.env.cr.fetchone()
 
         if result:                    
@@ -106,24 +107,3 @@ class WobinAdvances(models.Model):
         payment_related = self.env['account.payment'].search([('advance_id', '=', self.id)], limit=1).id 
         if payment_related:
             self.payment_related_id = payment_related
-
-
-    """
-    @api.one
-    @api.onchange('trip_id')
-    def _onchange_trip_id(self):  
-    #    self.ensure_one()  
-        ctxt = self.env.context
-        print('\n\n ??? context ???\n', ctxt)
-
-        if ctxt:
-            print('\n\n\n ??? context ID activo ???\n', ctxt['params']['id'])
-            advance_id = ctxt['params']['id']        
-
-        get_id_mvs = self.env['wobin.moves.adv.set.lines'].search([('advance_id', '=', self._origin.id)])
-        print('\n\n\n\n get_id_mvs?', get_id_mvs)
-        mov_lns_obj = self.env['wobin.moves.adv.set.lines'].browse(get_id_mvs.id)
-        print('\n\n\n\n mov_lns_obj?', mov_lns_obj)
-        mov_lns_obj.write({'trip_id': self.trip_id.id}) 
-
-    """
