e076a898793e2c0b5b65c0950ff9bb5f879fb4c9
Noe Moreno <nmoreno@wobin.com.mx>
2021-03-22 13:10:45 -0600

    [ADD] reporte

--- fertinova_addons/models/__init__.py
+++ fertinova_addons/models/__init__.py
@@ -6,4 +6,4 @@ from . import sale_inherited
 from . import stock_move_inherited
 from . import stock_quant_inherited
 from . import account_diot
-#from . import account_report
+from . import account_report
--- fertinova_addons/models/account_report.py
+++ fertinova_addons/models/account_report.py
@@ -1,4 +1,4 @@
-
+from odoo import models, fields, api
 
 class ReceiptUninvoiced(models.TransientModel):
     #Recepciones por facturar    
@@ -15,17 +15,32 @@ class ReportReceiptUninvoiced(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         report = self.env['account.lines'].browse(docids)
         lines = self.env['account.move.line'].search([('date','>',report.init_date),('date','<',report.end_date)])
-        
+        accounts = []
+
+        for line in lines:
+            found = False
+            for ac in accounts:
+                if line.account_id.id == ac:
+                    found = True
+            if found == False:
+                accounts.append(line.account_id.id)
+
+        new_lines = []
+
+        for account in accounts:
+            for line in lines:
+                if line.account_id.id == account:
+                    new_lines.append(line)
+
 
         report_data = {
             'i_date' : report.init_date.strftime("%d/%m/%Y"),
             'e_date': report.end_date.strftime("%d/%m/%Y"),
-            'today' : date.today(),
         }
 
         return {
             'doc_ids': docids,
             'doc_model': 'res.partner',
             'report_data' : report_data,
-            'lines' : lines
+            'lines' : new_lines
         }
