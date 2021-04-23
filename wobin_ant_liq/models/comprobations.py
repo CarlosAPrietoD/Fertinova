a8816e4a840629e3bad43d858bd0e3c37a8b0964
Sebastian-AM-Wobin <smendez@wobin.com.mx>
2021-04-23 12:40:42 -0500

    [ERR] Update y search en comprobations para ver si actualiza

--- wobin_ant_liq/models/comprobations.py
+++ wobin_ant_liq/models/comprobations.py
@@ -185,7 +185,7 @@ class WobinComprobations(models.Model):
     @api.onchange('operator_id')
     def _onchange_operator_id(self):        
         mov_lns_ad_set_obj = self.env['wobin.moves.adv.set.lines'].search([('id', '=', self.mov_lns_ad_set_id_aux.id)])
-        mov_lns_ad_set_obj.operator_id = self.operator_id.id                   
+        mov_lns_ad_set_obj.update({'operator_id': self.operator_id.id})
 
 
 
