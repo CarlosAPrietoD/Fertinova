04fce9fd1838ae66706be7aeb95339a842aea97d
Sebastian-AM-Wobin <smendez@wobin.com.mx>
2021-04-27 11:34:56 -0500

    [ERR] Prueba con instancia de id de self

--- wobin_ant_liq/models/advances.py
+++ wobin_ant_liq/models/advances.py
@@ -91,19 +91,15 @@ class WobinAdvances(models.Model):
         #Override write method in order to detect fields changed:
         res = super(WobinAdvances, self).write(vals)  
 
-        _logger.info('\n\n\n vals[mov_lns_aux_id]: %s\n\n\n', vals['mov_lns_aux_id'])
-
-        
         #If in fields changed are operator_id and trip_id update 
         #that data in its respective wobin.moves.adv.set.lines rows:
         if vals.get('operator_id', False):
             _logger.info('\n\n\n VALS: %s\n\n\n', vals)
-            mov_lns_obj = self.env['wobin.moves.adv.set.lines'].search([('id', '=', vals['mov_lns_aux_id'])])
-            _logger.info('\n\n\n mov_lns_obj: %s\n\n\n', mov_lns_obj)
+            _logger.info('\n\n\n self.mov_lns_aux_id.id: %s\n\n\n', self.mov_lns_aux_id)
             
-            if mov_lns_obj:
-                mov_lns_obj.operator_id = vals['operator_id']
-                _logger.info('\n\n\n mov_lns_obj.operator_id: %s\n\n\n', mov_lns_obj.operator_id)
+            if self.mov_lns_aux_id:
+                self.mov_lns_aux_id.operator_id = vals['operator_id']
+                _logger.info('\n\n\n Mov_lns_aux_id.operator_id: %s\n\n\n', self.mov_lns_aux_id.operator_id)
 
         if vals.get('trip_id', False):
             _logger.info('\n\n\n VALS: %s\n\n\n', vals)
