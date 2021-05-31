43800e326d7b6d14deea4ef0c75bcbd5c9df1f14
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2021-05-31 13:10:44 -0500

    Update __init__.py

--- fertinova_addons/models/__init__.py
+++ fertinova_addons/models/__init__.py
@@ -6,6 +6,7 @@ from . import account_report
 #from . import payments
 from . import product_template_inherited
 from . import purchase_inherited
+from . import res_partner_inherit
 from . import sale_inherited
 from . import stock_move_inherited
 from . import stock_quant_inherited
